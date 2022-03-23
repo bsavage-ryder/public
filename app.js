5c1dd7a48425d5db46bf5889d871b98ebbf085c4
bsavage-ryder <93536823+bsavage-ryder@users.noreply.github.com>
2022-03-23 10:44:44 -0400

    test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-console.log("START FROM GIT 9");
+console.log("START FROM GIT 010");
 
     // Create styles for form
     if (location.pathname == "/landing-test/form-test") {
 
         // Create styles for form
         var styDiv = document.createElement('div');
-        styDiv.innerHTML = '<style>div#formOverlayBGContainer{background:rgba(0,0,0,.5);position:fixed!important;z-index:9000;left:0;top:0;bottom:0;right:0;width:100%;transition:background .7s cubic-bezier(.55,.085,.68,.53)}div#formOverlayContainer{top:4.5rem;height:fit-content;position:absolute!important;z-index:9001;background:#fff;background:hsla(0,0%,100%,.9);bottom:18px;bottom:1.5rem;left:0;margin:auto;overflow:auto;right:0;top:72px;-ms-transform:translateX(10%);transform:translateX(10%);width:90%;transition:transform .7s cubic-bezier(.55,.085,.68,.53),opacity .7s cubic-bezier(.55,.085,.68,.53);margin-top:auto;opacity:1;-ms-transform:translateX(0);transform:translateX(0);transition:transform .5s cubic-bezier(.55,.085,.68,.53),opacity .7s cubic-bezier(.55,.085,.68,.53),background .7s cubic-bezier(.55,.085,.68,.53)}</style>';
+        styDiv.innerHTML = '<style>div#formOverlayBGContainer{display:none;background:rgba(0,0,0,.5);position:fixed!important;z-index:9000;left:0;top:0;bottom:0;right:0;width:100%;transition:background .7s cubic-bezier(.55,.085,.68,.53)}div#formOverlayContainer{top:4.5rem;height:fit-content;position:absolute!important;z-index:9001;background:#fff;background:hsla(0,0%,100%,.9);bottom:18px;bottom:1.5rem;left:0;margin:auto;overflow:auto;right:0;top:72px;-ms-transform:translateX(10%);transform:translateX(10%);width:90%;transition:transform .7s cubic-bezier(.55,.085,.68,.53),opacity .7s cubic-bezier(.55,.085,.68,.53);margin-top:auto;opacity:1;-ms-transform:translateX(0);transform:translateX(0);transition:transform .5s cubic-bezier(.55,.085,.68,.53),opacity .7s cubic-bezier(.55,.085,.68,.53),background .7s cubic-bezier(.55,.085,.68,.53)}</style>';
         document.body.appendChild(styDiv);
 
         // Create form Button for form
@@ -74,7 +74,7 @@ console.log("START FROM GIT 9");
         form.appendChild(createFormEl("ILANDPAGE", "", "hidden", ""));
         form.appendChild(createFormEl("IREFERRER", "IREFERRER", "hidden", ""));
         form.appendChild(createFormEl("LREFERRER", "LREFERRER", "hidden", ""));
-        form.appendChild(createFormEl("EngagementType", "", "hidden", ""));
+        form.appendChild(createFormEl("EngagementType", "", "hidden", "contact me"));
         form.appendChild(createFormEl("LPID", "LPID", "hidden", ""));
         form.appendChild(createFormEl("LoB", "LoB", "hidden", ""));
         form.appendChild(createFormEl("source", "source", "hidden", ""));
@@ -84,11 +84,11 @@ console.log("START FROM GIT 9");
 
         document.getElementById("EngagementType").value = document.location.pathname.split("/")[2];
         document.getElementById("LoB").value = document.location.pathname.split("/")[1];
-        //document.getElementById("ILANDPAGE").value = getCookie("_uc_initial_landing_page");
-        //document.getElementById("IREFERRER").value = getCookie("__lotl");
-        //document.getElementById("LREFERRER").value = getCookie("_uc_last_referrer");
+        document.getElementById("ILANDPAGE").value = getCookie("_uc_initial_landing_page");
+        document.getElementById("IREFERRER").value = getCookie("__lotl");
+        document.getElementById("LREFERRER").value = getCookie("_uc_last_referrer");
         document.getElementById("FormpageURL").value = location.pathname;
-        //document.getElementById("UDEVICE").value = deviceInfo();
+        document.getElementById("UDEVICE").value = deviceInfo();
 
     }
 
@@ -155,4 +155,77 @@ console.log("START FROM GIT 9");
         getsfid();
     }
 
-console.log("END FROM GIT 9");
+    function getCookie(cookieName) {
+        var name = cookieName + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if ((c.indexOf(name)) == 0) { return c.substr(name.length); }
+        }
+        return null;
+    }
+
+    // Logic for Warehouse Gated
+    function getsfid() {
+        var sfid_gdn = "7014X000002P58iQAC";
+        var sfid_psearch = "7014X000002P59MQAS";
+        var sfid_linkedin = "7014X000002P5A0QAK";
+        var sfid_facebook = "7014X000002P58jQAC";
+        var sfid_email = "7014X000002P592QAC";
+        if (source == "gdn") {
+            document.getElementById("fe27666").value = sfid_gdn;
+        } else if (source == "psearch") {
+            document.getElementById("fe27666").value = sfid_psearch;
+        } else if (source == "linkedin") {
+            document.getElementById("fe27666").value = sfid_linkedin;
+        } else if (source == "facebook") {
+            document.getElementById("fe27666").value = sfid_facebook;
+        } else if (source == "email") {
+            document.getElementById("fe27666").value = sfid_email;
+        } else {
+            document.getElementById("fe27666").value = " "
+        }
+    }
+
+    var timerId = null, timeout = 5;
+
+    function WaitUntilCustomerGUIDIsRetrieved() {
+        if (!!(timerId)) {
+            if (timeout == 0) {
+                return;
+            }
+            if (typeof this.GetElqCustomerGUID === 'function') {
+                document.forms["copyOf2021USELQUVSRTLForm-1645212545248"].elements["elqCustomerGUID"].value = GetElqCustomerGUID();
+                return;
+            }
+            timeout -= 1;
+        }
+        timerId = setTimeout("WaitUntilCustomerGUIDIsRetrieved()", 500);
+        return;
+    }
+    window.onload = WaitUntilCustomerGUIDIsRetrieved;
+    _elqQ.push(['elqGetCustomerGUID']);
+
+    function deviceInfo() {
+        if (window.navigator.userAgent.match(/Mobile/i)
+            || window.navigator.userAgent.match(/iPhone/i)
+            || window.navigator.userAgent.match(/iPod/i)
+            || window.navigator.userAgent.match(/IEMobile/i)
+            || window.navigator.userAgent.match(/Windows Phone/i)
+            || window.navigator.userAgent.match(/Android/i)
+            || window.navigator.userAgent.match(/BlackBerry/i)
+            || window.navigator.userAgent.match(/webOS/i)) {
+            return ('Mobile - ' + navigator.userAgent);
+        } else if (window.navigator.userAgent.match(/Tablet/i)
+            || window.navigator.userAgent.match(/iPad/i)
+            || window.navigator.userAgent.match(/Nexus 7/i)
+            || window.navigator.userAgent.match(/Nexus 10/i)
+            || window.navigator.userAgent.match(/KFAPWI/i)) {
+            return ('Tablet - ' + navigator.userAgent);
+        } else {
+            return ('Desktop - ' + navigator.userAgent);
+        }
+    }
+
+
+console.log("END FROM GIT 010");
